5000218abdd4637e8b5e47c260dd25142fd3725f
Tom Seifert <syex@memorian-it.de>
2016-01-12 12:32:47 +0100

    - Made this an Android Studio project. - Added Material library. - Extracted the sample into a separate module. - Code cleanup.

---
+++ app/src/main/java/com/androidbuts/multispinnerfilter/KeyPairBoolData.java
@@ -0,0 +1,50 @@
+package com.androidbuts.multispinnerfilter;
+
+public class KeyPairBoolData {
+
+    int id;
+    String name;
+    boolean isSelected;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the isSelected
+     */
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    /**
+     * @param isSelected the isSelected to set
+     */
+    public void setSelected(boolean isSelected) {
+        this.isSelected = isSelected;
+    }
+}
