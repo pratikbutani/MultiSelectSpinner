f29d66f30873b7fce77fbc93051e2b2feb4e5d2a
Pratik Butani <pratik13butani@gmail.com>
2020-09-10 18:11:28 +0530

    Object Param Added

--- library/src/main/java/com/androidbuts/multispinnerfilter/KeyPairBoolData.java
+++ library/src/main/java/com/androidbuts/multispinnerfilter/KeyPairBoolData.java
@@ -1,49 +1,58 @@
 package com.androidbuts.multispinnerfilter;
 
 public class KeyPairBoolData {
-    private long id;
-    private String name;
-    private boolean isSelected;
+	private long id;
+	private String name;
+	private boolean isSelected;
+	private Object object;
 
-    /**
-     * @return the id
-     */
-    public long getId() {
-        return id;
-    }
+	public Object getObject() {
+		return object;
+	}
 
-    /**
-     * @param id the id to set
-     */
-    public void setId(long id) {
-        this.id = id;
-    }
+	public void setObject(Object object) {
+		this.object = object;
+	}
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
 
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    /**
-     * @return the isSelected
-     */
-    public boolean isSelected() {
-        return isSelected;
-    }
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
 
-    /**
-     * @param isSelected the isSelected to set
-     */
-    public void setSelected(boolean isSelected) {
-        this.isSelected = isSelected;
-    }
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the isSelected
+	 */
+	public boolean isSelected() {
+		return isSelected;
+	}
+
+	/**
+	 * @param isSelected the isSelected to set
+	 */
+	public void setSelected(boolean isSelected) {
+		this.isSelected = isSelected;
+	}
 }
